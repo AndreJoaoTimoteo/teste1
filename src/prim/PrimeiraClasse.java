7ac8913de98d402f51abbb1d34d3350937dadab3
AndreTimoteo <andrejoao.timoteo@gmail.com>
2019-06-24 17:18:47 +0100

    update ao primeiraclasse

--- src/prim/PrimeiraClasse.java
+++ src/prim/PrimeiraClasse.java
@@ -4,7 +4,7 @@ public class PrimeiraClasse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Nossa primeira classe.");
+		System.out.println("Minha primeira classe.");
 	}
 
 }
