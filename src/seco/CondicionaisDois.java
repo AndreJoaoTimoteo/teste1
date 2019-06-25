3bff4de67f7ac48112f31a960ddba047043ea9e5
AndreTimoteo <andrejoao.timoteo@gmail.com>
2019-06-25 17:16:52 +0100

    muh condishuns

---
+++ src/seco/CondicionaisDois.java
@@ -0,0 +1,17 @@
+package seco;
+
+public class CondicionaisDois {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value1 = 1;
+		int value2 = 2;
+		int result;
+		boolean someCondition = true;
+		
+		result = someCondition ? value1 : value2;
+		
+		System.out.println(result);
+	}
+
+}
