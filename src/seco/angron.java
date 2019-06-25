12646b2db892264cbcc5949ec6c16106ad85bda6
AndreTimoteo <andrejoao.timoteo@gmail.com>
2019-06-25 10:29:17 +0100

    Tipos e casts

---
+++ src/seco/angron.java
@@ -0,0 +1,29 @@
+package seco;
+
+public class angron {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**double valor = 3.1415;
+		int i = valor;
+		System.out.println(i);
+		int j = 3.14;
+		
+		
+		double kharn = 5;
+		int argeltal = kharn;
+		*/
+		long erebus = 10000;
+		//int korphaeron = erebus;
+		System.out.println(erebus);
+		
+		int magnus = 5;
+		double didnothingwrong = magnus;
+		System.out.println(didnothingwrong);
+		
+		long cat = 1000000000032323000L;
+		int dog = (int) cat;
+		System.out.println(cat+" e "+dog);
+	}
+
+}
---
+++ src/seco/lorgar.java
@@ -0,0 +1,42 @@
+package seco;
+
+public class lorgar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer bu = new Integer(1);
+		Integer ub = new Integer("23");
+		System.out.println(bu+" e "+ub);
+		
+		Boolean boop = new Boolean(true);
+		Boolean poob = new Boolean("false");
+		System.out.println(boop+" e "+poob);
+		
+		Byte bi = new Byte((byte)3);
+		Byte ib = new Byte("-3");
+		System.out.println(bi+" e "+ib);
+		
+		Character letra = new Character('c');
+		System.out.println(letra);
+		
+		Double trouble = new Double(3.4);
+		Double elbuort = new Double("4.3");
+		System.out.println(trouble+" e "+elbuort);
+		
+		Float weAll = new Float(7.47F);
+		Float llAew = new Float("4.74");
+		System.out.println(weAll+" e "+llAew);
+		
+		Long johnson = new Long(1337L);
+		Long nosnhoj = new Long("50135");
+		System.out.println(johnson+" e "+nosnhoj);
+		
+		Short cake = new Short((short) 21);
+		Short ekac = new Short("12");
+		System.out.println(cake+" e "+ekac);
+		
+		
+		
+	}
+
+}
