ce819c6b69465ff28926ae00c2b8ede9286c7c1e
AndreTimoteo <andrejoao.timoteo@gmail.com>
2019-06-25 16:56:25 +0100

    aritmetico redone

--- src/seco/Aritmetico.java
+++ src/seco/Aritmetico.java
@@ -20,7 +20,11 @@ public class Aritmetico {
 		result = result / 2;
 		System.out.println(result);
 		
-		//result is now 2
+		//result is now 10
+		result = result +8;
+		System.out.println(result);
+		
+		//result is now 3
 		result = result % 7;
 		System.out.println(result);
 	}
