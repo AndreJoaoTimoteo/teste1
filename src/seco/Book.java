4e674bab72f571b67c3664021dcad7934f05f05a
AndreTimoteo <andrejoao.timoteo@gmail.com>
2019-06-25 15:38:43 +0100

    Book takes equals()

--- src/seco/Book.java
+++ src/seco/Book.java
@@ -3,11 +3,12 @@ package seco;
 public class Book {
 
 	private String isbn;
-	
-	public Book (String isbn) {
-		this.setIsbn(isbn);
+		
+	public Book(String isbn) {
+		super();
+		this.isbn = isbn;
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String is = "Horus";
@@ -24,5 +25,31 @@ public class Book {
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((isbn == null) ? 0 : isbn.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (isbn == null) {
+			if (other.isbn != null)
+				return false;
+		} else if (!isbn.equals(other.isbn))
+			return false;
+		return true;
+	}
+	
 
 }
