eb55a99c66fdbb9dd127007ba1f225ef00e13482
AndreTimoteo <andrejoao.timoteo@gmail.com>
2019-06-25 11:12:58 +0100

    invocações de métodos <tipo>Value()

--- src/seco/lorgar.java
+++ src/seco/lorgar.java
@@ -7,36 +7,73 @@ public class lorgar {
 		Integer bu = new Integer(1);
 		Integer ub = new Integer("23");
 		System.out.println(bu+" e "+ub);
+		System.out.println(bu.byteValue());
+		System.out.println(bu.doubleValue());
+		System.out.println(bu.intValue());
+		System.out.println(bu.longValue());
+		System.out.println(bu.shortValue());
+		System.out.println(bu.floatValue());
 		
 		Boolean boop = new Boolean(true);
 		Boolean poob = new Boolean("false");
 		System.out.println(boop+" e "+poob);
+		System.out.println(boop.booleanValue());
 		
 		Byte bi = new Byte((byte)3);
 		Byte ib = new Byte("-3");
 		System.out.println(bi+" e "+ib);
+		System.out.println(bi.byteValue());
+		System.out.println(bi.doubleValue());
+
+		System.out.println(bi.intValue());
+		System.out.println(bi.longValue());
+		System.out.println(bi.shortValue());
+		System.out.println(bi.floatValue());
 		
 		Character letra = new Character('c');
 		System.out.println(letra);
-		
+		System.out.println(letra.charValue());
+
 		Double trouble = new Double(3.4);
 		Double elbuort = new Double("4.3");
 		System.out.println(trouble+" e "+elbuort);
+		System.out.println(trouble.byteValue());
+		System.out.println(trouble.doubleValue());
+		System.out.println(trouble.intValue());
+		System.out.println(trouble.longValue());
+		System.out.println(trouble.shortValue());
+		System.out.println(trouble.floatValue());
 		
 		Float weAll = new Float(7.47F);
 		Float llAew = new Float("4.74");
 		System.out.println(weAll+" e "+llAew);
+		System.out.println(weAll.byteValue());
+		System.out.println(weAll.doubleValue());
+		System.out.println(weAll.intValue());
+		System.out.println(weAll.longValue());
+		System.out.println(weAll.shortValue());
+		System.out.println(weAll.floatValue());
 		
 		Long johnson = new Long(1337L);
 		Long nosnhoj = new Long("50135");
 		System.out.println(johnson+" e "+nosnhoj);
+		System.out.println(johnson.byteValue());
+		System.out.println(johnson.doubleValue());
+		System.out.println(johnson.intValue());
+		System.out.println(johnson.longValue());
+		System.out.println(johnson.shortValue());
+		System.out.println(johnson.floatValue());
 		
 		Short cake = new Short((short) 21);
 		Short ekac = new Short("12");
 		System.out.println(cake+" e "+ekac);
-		
-		
-		
+		System.out.println(cake.byteValue());
+		System.out.println(cake.doubleValue());
+		System.out.println(cake.intValue());
+		System.out.println(cake.longValue());
+		System.out.println(cake.shortValue());
+		System.out.println(cake.floatValue());
+			
 	}
 
 }
