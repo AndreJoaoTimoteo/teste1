f6979a45b842993eea8356dd92d4685e6d8b3eb4
AndreTimoteo <andrejoao.timoteo@gmail.com>
2019-06-25 17:04:14 +0100

    majors with minors, someone call the cops

---
+++ src/seco/RelacionaisOuIgualdade.java
@@ -0,0 +1,28 @@
+package seco;
+
+public class RelacionaisOuIgualdade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int value1 = 1;
+		int value2 = 2;
+		
+		if (value1 == value2)
+			System.out.println(false);
+		
+		if (value1 != value2)
+			System.out.println(true);
+		
+		if (value1 > value2)
+			System.out.println(false);
+		
+		if (value1 < value2)
+			System.out.println(true);
+		
+		if (value1 <= value2)
+			System.out.println(true);
+		
+	}
+
+}
