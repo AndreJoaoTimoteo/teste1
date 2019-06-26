4702dad07cd0bf46df2dbb5907975784a4d0e2a7
AndreTimoteo <andrejoao.timoteo@gmail.com>
2019-06-26 11:15:30 +0100

    roda da sort

--- src/terc/sortir.java
+++ src/terc/sortir.java
@@ -2,15 +2,18 @@ package terc;
 
 import java.util.Arrays;
 
-public class gorilada {
+
+public class sortir {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		int[] array = new int[5];
-		for (int i=0;i<5;i++)
-			array[i]=i+1;
+		for (int i=0; i<5; i++)
+			array[i]=(int)(i*Math.pow(-1,i));
 		
-		System.out.println(array);
+		System.out.println(Arrays.toString(array));
+		Arrays.sort(array);
 		System.out.println(Arrays.toString(array));
 	}
 
