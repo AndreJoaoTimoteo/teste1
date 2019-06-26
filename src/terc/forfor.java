e5446ef3553b76f5364f28c4865dfb7a2f4bcea5
AndreTimoteo <andrejoao.timoteo@gmail.com>
2019-06-26 11:00:39 +0100

    forfor e gorilada

---
+++ src/terc/forfor.java
@@ -0,0 +1,22 @@
+package terc;
+
+import java.lang.*;
+import java.io.*;
+
+public class forfor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int NUM = 6;
+		for (int i = 0; i<NUM; i++)
+			System.out.println(X(i));
+	}
+
+	
+	public static int X(int n) {
+		int result = 1;
+		for (int i = 2; i <= n; i++)
+			result *= i;
+			return result;
+	}
+}
---
+++ src/terc/gorilada.java
@@ -0,0 +1,16 @@
+package terc;
+
+import java.util.Arrays;
+public class gorilada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = new int[5];
+		for (int i=0;i<5;i++)
+			array[i]=i+1;
+		
+		System.out.println(array);
+		System.out.println(Arrays.toString(array));
+	}
+
+}
