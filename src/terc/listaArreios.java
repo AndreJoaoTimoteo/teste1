8cfaf2b330a5a865fa707e10084499968165ab5b
AndreTimoteo <andrejoao.timoteo@gmail.com>
2019-06-26 13:54:07 +0100

    arraylistulos

---
+++ src/terc/listaArreios.java
@@ -0,0 +1,16 @@
+package terc;
+
+import java.util.ArrayList;
+
+public class listaArreios {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(new Integer(1));
+		list.add(2);
+		
+		System.out.println(list);
+	}
+
+}
